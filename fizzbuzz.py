72c12a78165ffa6ec4cdb4ca988bdc5a0b6e44bb
Ángel Álvarez <kreandostudio@gmail.com>
2018-12-17 00:25:15 +0100

    Mejoras

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,36 +3,53 @@
 
 fizz = None
 buzz = None
-#fizzbuzz = None
+fizzbuzz = True
+correcto = True
 
 print "!!!Bienvenidos a FizzBuzz!!! "
 
-try:
-    num = int(raw_input("Introduzca un numero: "))
-    if num > 0 and num <= 100:
-        for i in range(1, num + 1):
-            if i == 3:
-                # fizz = i % 3
-                # if fizz == 0:
-                print "Fizz"
-            elif i == 5:
-                # buzz = i % 5
-                # if buzz == 0:
-                print "Buzz"
-            elif i > 5:
-                fizz = i % 3
-                buzz = i % 5
-                if fizz == 0 and buzz == 0:
-                    print "FizzBuzz"
-                elif fizz == 0:
+while fizzbuzz:
+    try:
+        num = int(raw_input("Introduzca un número del 1 al 100: "))
+        if num > 0 and num <= 100:
+            correcto = True
+            for i in range(1, num + 1):
+                if i == 3:
+                    # fizz = i % 3
+                    # if fizz == 0:
                     print "Fizz"
-                elif buzz == 0:
+                elif i == 5:
+                    # buzz = i % 5
+                    # if buzz == 0:
                     print "Buzz"
+                elif i > 5:
+                    fizz = i % 3
+                    buzz = i % 5
+                    if fizz == 0 and buzz == 0:
+                        print "FizzBuzz"
+                    elif fizz == 0:
+                        print "Fizz"
+                    elif buzz == 0:
+                        print "Buzz"
+                    else:
+                        print i
                 else:
                     print i
-            else:
-                print i
-    else:
-        print "Por favor, no introduzca numeros negativos!"
-except ValueError:
-    print "Por favor, introduzca solo números."
+        else:
+            print "Por favor, introduzca un número válido!"
+            correcto = False
+    except ValueError:
+        print "Por favor, introduzca solo números."
+        correcto = False
+
+    #correcto = True
+    while  correcto:
+        pregunta = raw_input("¿Desea jugar de nuevo? (S/N): ")[0]
+        pregunta = pregunta.upper()
+        if pregunta == "S":
+            correcto = False
+        elif pregunta == "N":
+            correcto = False
+            fizzbuzz = False
+        else:
+            print "Opción incorrecta!"
